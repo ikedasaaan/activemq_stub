9fb5a0fa5396dfd3028d5c4afde659683d1113c5
ikeda <ikeda@it-tera.com>
2015-07-16 17:51:25 +0900

    deketa

--- src/main/java/com/MainApp.java
+++ src/main/java/com/MainApp.java
@@ -2,47 +2,88 @@ package com;
 
 import javax.jms.JMSException;
 import javax.jms.MapMessage;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Queue;
 import javax.jms.QueueConnection;
 import javax.jms.QueueConnectionFactory;
-import javax.jms.QueueReceiver;
 import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import com.util.Constants;
 
-public class MainApp {
+public class MainApp implements MessageListener {
 
-    public static void main(String[] args){
+	private Session session;
+
+
+	public static void main(String[] args) {
+		new MainApp();
+	}
+
+
+	/**
+	 * Constructor
+	 */
+	public MainApp() {
+		this.setupMessageQueueConsumer();
+	}
+
+	private void setupMessageQueueConsumer() {
+
+		//Connectionを作成するFactoryを作成
+		QueueConnectionFactory connectionFactory = new ActiveMQConnectionFactory(Constants.MSG_BROKER_URL);
 
         try {
             //Connectionを作成するFactoryを作成
-            QueueConnectionFactory connectionFactory = new ActiveMQConnectionFactory(Constants.MSG_BROKER_URL);
             QueueConnection connection = connectionFactory.createQueueConnection();
-
+            connection.start();
             //セッションの作成
-            QueueSession session = connection.createQueueSession(false,QueueSession.AUTO_ACKNOWLEDGE);
+            this.session = connection.createQueueSession(false,QueueSession.AUTO_ACKNOWLEDGE);
 
 
             Queue testQueue = session.createQueue("destination");
 
 
             //Queueと関連付け
-            QueueReceiver receiver = session.createReceiver(testQueue);
+			MessageConsumer consumer = this.session.createConsumer(testQueue);
+			consumer.setMessageListener(this);
 
-            connection.start();
 
-            //メッセージの受信
-            MapMessage msg = (MapMessage) receiver.receive();
-            System.out.println(msg.getMapNames());
-
-            receiver.close();
-            session.close();
-            connection.close();
+            //receiver.close();
+            //session.close();
+            //connection.close();
         } catch (JMSException e) {
             e.printStackTrace();
         }
 
     }
+
+
+	@Override
+	public void onMessage(Message message) {
+		// TODO 自動生成されたメソッド・スタブ
+		// terminate if JMSCorrelationID is not set
+		try {
+			if (message.getJMSCorrelationID() == null) {
+				System.out.println("JMSCorrelationID :" + message.getJMSCorrelationID());
+				return;
+			}
+			if (message instanceof TextMessage) {
+				TextMessage msg = (TextMessage)message;
+				System.out.println("[TextMessage]" + msg.getText());
+			} else if (message instanceof MapMessage) {
+				MapMessage msg = (MapMessage)message;
+				System.out.println("[MapMessage]" + msg.getMapNames());
+			}
+		} catch (JMSException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+	}
 }
